570a62908aee30b72f43e8d15410abd5a79058a9
Tiago Silva Costa <tiago.costa@ecologybrasil.com.br>
2019-06-28 18:22:39 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { AlertaService } from './alerta.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  title = 'Livro Angular 2';
+  foto: string = 'favicon.ico';
+
+  constructor(
+    private service: AlertaService
+  ){}
+
+  enviarMsg(): void {
+    this.service.msgAlerta();
+  }
 }
---
+++ src/app/lista-pessoa/lista-pessoa.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { PessoaServiceService } from './pessoa-service.service';
+
+@Component({
+  selector: 'app-lista-pessoa',
+  templateUrl: './lista-pessoa.component.html',
+  styleUrls: ['./lista-pessoa.component.css'],
+  providers: [PessoaServiceService]
+})
+export class ListaPessoaComponent implements OnInit {
+
+  pessoas: string [];
+  nome: string = 'Tiago';
+
+  constructor(
+    private service: PessoaServiceService
+  ) { 
+    this.pessoas = service.getPessoas();
+  }
+
+  ngOnInit() {
+  }
+
+  enviarNome() {
+    this.service.setPessoa(this.nome);
+  }
+
+  listar(){
+
+  }
+
+}
